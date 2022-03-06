9678c5d80c8a699d8c1148d0669ff2cba4007769
ShineStar <shinestar0803@gmail.com>
2022-03-06 08:00:53 +0300

    add condition loop

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+fn get_num(x: u32) -> i32 {
+    let ret_num = if x > 5 { 2 + 5 } else {1};
+    ret_num
+}
+
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("The secret number is: {}", secret_number);
+    for number in (1..=4).rev() {
+        println!("{}!", number);
+    }
 
     loop {
         println!("Please input your guess.");
@@ -24,6 +32,7 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue
         };
+        println!("the value {}", get_num(guess));
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("small"),
